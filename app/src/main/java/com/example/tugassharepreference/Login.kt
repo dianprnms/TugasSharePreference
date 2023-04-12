ecba01aa728bf46981ee204ec2a9b5a706624d0a
dianprnms <purnamasaridian010@gmail.com>
2023-04-13 00:16:51 +0700

    update register

--- app/src/main/java/com/example/tugassharepreference/Login.kt
+++ app/src/main/java/com/example/tugassharepreference/Login.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.tugassharepreference.databinding.FragmentLoginBinding
 
 
 class Login : Fragment() {
+    lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ):
+            View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding= FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnLogin.setOnClickListener{
+            val bundle = Bundle()
+            bundle.putString("Username", binding.UsernameLogin.text.toString())
+            findNavController().navigate(R.id.action_login_to_register, bundle)
+        }
     }
 
 
